2a893664a06cde676be14efa2b0d6a7fc0e67026
kenorb <kenorb@users.noreply.github.com>
2023-05-29 16:45:21 +0100

    Copies strategies.h into includes.h

---
+++ includes.h
@@ -0,0 +1,70 @@
+//+------------------------------------------------------------------+
+//|                  EA31337 - multi-strategy advanced trading robot |
+//|                                 Copyright 2016-2021, EA31337 Ltd |
+//|                                       https://github.com/EA31337 |
+//+------------------------------------------------------------------+
+
+/*
+ *  This file is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+// Includes strategy classes.
+#include "AC/Stg_AC.mqh"
+#include "AD/Stg_AD.mqh"
+#include "ADX/Stg_ADX.mqh"
+#include "AMA/Stg_AMA.mqh"
+#include "ASI/Stg_ASI.mqh"
+#include "ATR/Stg_ATR.mqh"
+#include "ATR_MA_Trend/Stg_ATR_MA_Trend.mqh"
+#include "Alligator/Stg_Alligator.mqh"
+#include "Awesome/Stg_Awesome.mqh"
+#include "BWMFI/Stg_BWMFI.mqh"
+#include "Bands/Stg_Bands.mqh"
+#include "BearsPower/Stg_BearsPower.mqh"
+#include "BullsPower/Stg_BullsPower.mqh"
+#include "CCI/Stg_CCI.mqh"
+#include "Chaikin/Stg_Chaikin.mqh"
+#include "DEMA/Stg_DEMA.mqh"
+#include "DeMarker/Stg_DeMarker.mqh"
+#include "ElliottWave/Stg_ElliottWave.mqh"
+#include "Envelopes/Stg_Envelopes.mqh"
+#include "Force/Stg_Force.mqh"
+#include "Fractals/Stg_Fractals.mqh"
+#include "Gator/Stg_Gator.mqh"
+#include "HeikenAshi/Stg_HeikenAshi.mqh"
+#include "Ichimoku/Stg_Ichimoku.mqh"
+#include "Indicator/Stg_Indicator.mqh"
+#include "MA/Stg_MA.mqh"
+#include "MACD/Stg_MACD.mqh"
+#include "Meta_Multi/Stg_Meta_Multi.mqh"
+#include "MFI/Stg_MFI.mqh"
+#include "Momentum/Stg_Momentum.mqh"
+#include "OBV/Stg_OBV.mqh"
+#include "OSMA/Stg_OSMA.mqh"
+#include "Pattern/Stg_Pattern.mqh"
+#include "Pinbar/Stg_Pinbar.mqh"
+#include "Pivot/Stg_Pivot.mqh"
+#include "RSI/Stg_RSI.mqh"
+#include "RVI/Stg_RVI.mqh"
+#include "SAR/Stg_SAR.mqh"
+#include "SAWA/Stg_SAWA.mqh"
+#include "SVE_Bollinger_Bands/Stg_SVE_Bollinger_Bands.mqh"
+#include "StdDev/Stg_StdDev.mqh"
+#include "SuperTrend/Stg_SuperTrend.mqh"
+#include "Stochastic/Stg_Stochastic.mqh"
+#include "TMAT_SVEBB/Stg_TMAT_SVEBB.mqh"
+#include "TMA_CG/Stg_TMA_CG.mqh"
+#include "TMA_True/Stg_TMA_True.mqh"
+#include "WPR/Stg_WPR.mqh"
+#include "ZigZag/Stg_ZigZag.mqh"
