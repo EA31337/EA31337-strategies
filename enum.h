1ecd3f1cfb1d16fa17c7e757b902c5b322c62238
kenorb <kenorb@users.noreply.github.com>
2023-10-24 20:14:01 +0100

    Removes enum items for meta strategies in favor of dedicated enum

--- enum.h
+++ enum.h
@@ -60,40 +60,6 @@ enum ENUM_STRATEGY {
   STRAT_MA_CROSS_TIMEFRAME,      // MA Cross Timeframe
   STRAT_MA_TREND,                // MA Trend
   STRAT_MACD,                    // MACD
-  STRAT_META_BEARS_BULLS,        // (Meta) Bears & Bulls
-  STRAT_META_DOUBLE,             // (Meta) Double
-  STRAT_META_CONDITIONS,         // (Meta) Conditions
-  STRAT_META_ENHANCE,            // (Meta) Enhance
-  STRAT_META_EQUITY,             // (Meta) Equity
-  STRAT_META_FORMATION,          // (Meta) Formation
-  STRAT_META_INTERVAL,           // (Meta) Interval
-  STRAT_META_HEDGE,              // (Meta) Hedge
-  STRAT_META_LIMIT,              // (Meta) Limit
-  STRAT_META_MA_CROSS,           // (Meta) MA Cross
-  STRAT_META_MARGIN,             // (Meta) Margin
-  STRAT_META_MARTINGALE,         // (Meta) Martingale
-  STRAT_META_MIRROR,             // (Meta) Mirror
-  STRAT_META_MULTI,              // (Meta) Multi
-  STRAT_META_MULTI_CURRENCY,     // (Meta) Multi Currency
-  STRAT_META_NEWS,               // (Meta) News
-  STRAT_META_ORDER_LIMIT,        // (Meta) Order Limit
-  STRAT_META_OSCILLATOR_FILTER,  // (Meta) Oscillator Filter
-  STRAT_META_OSCILLATOR_SWITCH,  // (Meta) Oscillator Switch
-  STRAT_META_PATTERN,            // (Meta) Pattern
-  STRAT_META_PIVOT,              // (Meta) Pivot
-  STRAT_META_PROFIT,             // (Meta) Profit
-  STRAT_META_RESISTANCE,         // (Meta) Resistance
-  STRAT_META_REVERSAL,           // (Meta) Reversal
-  STRAT_META_RISK,               // (Meta) Risk
-  STRAT_META_RSI,                // (Meta) RSI
-  STRAT_META_SCALPER,            // (Meta) Scalper
-  STRAT_META_SIGNAL_SWITCH,      // (Meta) Signal Switch
-  STRAT_META_SPREAD,             // (Meta) Spread
-  STRAT_META_TIMEZONE,           // (Meta) Timezone
-  STRAT_META_TREND,              // (Meta) Trend
-  STRAT_META_TRIO,               // (Meta) Trio
-  STRAT_META_VOLATILITY,         // (Meta) Volatility
-  STRAT_META_WEEKDAY,            // (Meta) Weekday
   STRAT_MFI,                     // MFI
   STRAT_MOMENTUM,                // Momentum
   STRAT_OBV,                     // OBV
